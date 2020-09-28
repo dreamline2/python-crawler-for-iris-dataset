69fabf831991bf592c90e6855eea6cedd62899ef
human <human@chenjingweideMacBook-Pro.local>
2020-09-28 16:53:39 +0800

    fixed encoding issue from TWSE

--- stock.py
+++ stock.py
@@ -1,24 +1,23 @@
 #!/user/bin/env python3
-# -*- coding: utf-8 -*-
 import csv
 import matplotlib.pyplot as plt
 fn = 'stock.csv'
-with open(fn) as f:
+with open(fn, encoding="ISO-8859-1") as f:
     csvRead = csv.reader(f)
     csvList = list(csvRead)  # 轉成串列
     csvData = csvList[2:-4]
     months, highs, lows, prices = [], [], [], []
     for row in csvData:
-            highs.append(float(row[2]))
-            lows.append(float(row[3]))
-            prices.append(float(row[4]))
-            months.append(int(row[1]))
+        highs.append(float(row[2]))
+        lows.append(float(row[3]))
+        prices.append(float(row[4]))
+        months.append(int(row[1]))
 fig = plt.figure(dpi=80, figsize=(12, 8))
 plt.plot(months, highs, '-*', label='High')
 plt.plot(months, lows, '-o', label='Low')
 plt.plot(months, prices, '-^', label='Price')
 plt.legend(loc='upper center')
-#fig.autofmt_xdate()
+# fig.autofmt_xdate()
 plt.title("TSMC", fontsize=24)
 plt.xlabel("Month", fontsize=14)
 plt.ylabel("Price", fontsize=14)
